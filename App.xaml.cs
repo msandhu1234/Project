7ef84e42b23d897e4cb7a1b95690266e7b34bf7c
msandhu1234 <ms1803927@gmail.com>
2024-11-05 13:44:10 -0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TaskManagementApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
